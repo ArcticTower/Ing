b9bd60362fded866d7287fee8fa634e0a346158a
ArcticTower <perf@live.ru>
2022-02-22 19:21:41 +0300

    35sForBig

--- src/main/java/org/ing/Ing.java
+++ src/main/java/org/ing/Ing.java
@@ -14,7 +14,7 @@ public class Ing {
 
         long startTime = System.nanoTime();
         String fileName;
-        String reg = "^\"\\d+\"$";
+        String reg = "^\"\\d+\\.\\d+\"$";
 
         if (args.length<1){
             System.out.println("Usage:");
@@ -50,93 +50,59 @@ public class Ing {
             System.out.println("File not found!");
             return;
         }
+
         String line;
-        String token;
-
-//        HashSet<Group> groups = new HashSet<>();
-//        ArrayList<HashSet<String>> groups = new ArrayList<>();
-//        HashMap<String,Group> map = new HashMap<>();
-//        ArrayList<ArrayList<HashSet>> groups = new ArrayList<>();
-//        HashMap<String,ArrayList<HashSet<String>>> map = new HashMap<>();
-
-        ArrayList<String> strings_list = new ArrayList<>();
-        HashMap <String,HashSet<String>> map = new HashMap<>();
-//        SortedMap<String,HashSet<String>> map = new
-
-
-
+//        StringFilter stringFilter = new StringFilter();
+        HashMap<String, Node>map = new HashMap<>();
 
         try{
-
-
-
             while ((line = bufferedReader.readLine()) != null){
-                StringBuilder stringB;
-                HashSet <String> tokens;
-                boolean validline = true;
-                //read by line
-                if(line.length()>0){//if line not empty
-                    StringTokenizer tokenizer = new StringTokenizer(line,";",false);
-                    if(tokenizer.countTokens()<2){//and contains more than 1 element
+                if(line.length()<3) continue;
+                StringTokenizer tokenizer = new StringTokenizer(line,";");
+                int count = tokenizer.countTokens();
+                if(count<2) continue;
+                HashSet<String> tokens_to_link = new HashSet<>();
+                ///todo sort string to node
+                HashSet<Node> links = new HashSet<>();
+                HashSet<String> sb = new HashSet<>();
+                for(int i =0;i<count;i++){
+                    String tok = tokenizer.nextToken();
+
+                    if(!tok.matches(reg)){
                         continue;
                     }
-                     stringB = new StringBuilder();
-                     tokens = new HashSet<>();
-                    while (tokenizer.hasMoreTokens()){
-                        token = tokenizer.nextToken();
-                        if(token.length()<3){
-                            validline = false;
-                            continue;
-                        }
-                        if(!token.matches(reg)){
-                            validline=false;
-                            continue;
-                        }
-                        if(!validline) continue;
-                        StringBuilder builder = new StringBuilder(token);
-                        builder.deleteCharAt(token.length()-1);
-                        builder.deleteCharAt(0);
-                        token = builder.toString();
-                        stringB.append(token);
-                        stringB.append(" ");
-                        tokens.add(token);
-                        ///todo
+                    StringBuilder stringBuilder = new StringBuilder(tok);
+                    stringBuilder.deleteCharAt(tok.length()-1);
+                    stringBuilder.deleteCharAt(0);
+                    tok = stringBuilder.toString();
 
+                    sb.add(tok);
+                    if(map.containsKey(tok)){
+                        links.add(map.get(tok));
+                    }else {
+                        tokens_to_link.add(tok);
                     }
-                }else {
-                    continue;
                 }
 
-
-
-                if(validline){
-
-                    String str = stringB.toString();
-                    strings_list.add(str);
-
-                    for (String t : tokens){
-                        if(map.containsKey(t)){
-                            HashSet<String> tmp = map.get(t);
-                            tmp.add(str);
-                        }else {
-                            HashSet<String> set = new HashSet<>();
-                            set.add(str);
-                            map.put(t, set);
-                        }
+                Node node = new Node(sb);
+                if(links.size()>0){
+                    for (Node n : links){
+                        n.connect(node);
                     }
-
-                }else {
-//                    continue;
                 }
-
-
+                if(tokens_to_link.size()>0){
+                    for( String tok : tokens_to_link){
+                        map.put(tok,node);
+                    }
+                }
 
             }
         } catch (IOException e) {
-            System.out.println("File IO broke!");
             e.printStackTrace();
             return;
         }
+
+
         String resultFileName = fileName + "_result.txt";
         File resultFile = new File(resultFileName);
         if(resultFile.exists()){
@@ -157,156 +123,67 @@ public class Ing {
             e.printStackTrace();
             return;
         }
-        long bigGroupCounter =0;
-
-        //form groups
-//        HashSet<String>used = new HashSet<>();
-        HashMap<String,Integer>link = new HashMap<>();
-//        LinkedList<ArrayList<String>> groups = new LinkedList<>();
-//        Map<String,ArrayList<String>>dic = new HashMap<>();
-        ArrayList<ArrayList<String>>dic = new ArrayList<>();
-
-        for (Map.Entry<String, HashSet<String>> entry : map.entrySet()) {
-            HashSet<String> values = entry.getValue();
-            HashSet<Integer>back_link = new HashSet<>();
 
 
-            for (String v : values){
-                if(link.containsKey(v)) {
-                    int lnk = link.get(v);
-                    if(lnk == 1336){
-                        System.out.println(lnk);
-                        System.out.println(dic.get(1336));
-                        System.out.println(values);
-                    }
-                    back_link.add(lnk);
-                }
-            }
+        HashSet<Node> groupNodes;
+//        ArrayList<Integer,ArrayList<Node>> sizeLinks = new ArrayList<>();
+//        Map<Integer, HashSet<HashSet<String>>> sizeMap = new TreeMap<>();
+        ArrayList<HashSet<HashSet<String>>> sizedList = new ArrayList<>();
 
-            if (back_link.size()>=2){
-//                System.out.println("WHY");
-            }
-            if (back_link.size()>0){//add to group
-                ////todo
+        for (Map.Entry<String,Node> entry : map.entrySet()){
 
-                Integer l = new ArrayList<Integer>(back_link).get(0);
-                for (String v : values){
-                    link.put(v ,l);
-                }
-                ArrayList<String> ob = dic.get(l);//todo array to hashset
-                ob.addAll(values);
-            }
-            if(back_link.size()<1){//create group
-                ArrayList<String> newDicEl = new ArrayList<>(values);
-                int l;
-                l = dic.size();
-                dic.add(newDicEl);
-
-//                String l = newDicEl.get(0);
-//                dic.add(l);
-//                dic.put(l,newDicEl);
-                for(String v :values){
-                    link.put(v,l);
-                }
+            Node n = entry.getValue();
+            if(!n.isWrapped()){
+                HashSet<HashSet<String>> group = new HashSet<>();
+                n.getConnectList(group);
+                sizedList.add(group);
             }
 
         }
 
-        Collections.sort(dic, new Comparator<ArrayList<String>>() {
+        Collections.sort(sizedList, new Comparator<HashSet<HashSet<String>>>() {
             @Override
-            public int compare(ArrayList<String> strings, ArrayList<String> t1) {
-                if(strings.size()==t1.size()) return 0;
-                if(strings.size()>t1.size()) return -1;
+            public int compare(HashSet<HashSet<String>> hashSets, HashSet<HashSet<String>> t1) {
+                if(hashSets.size() == t1.size())return 0;
+                if(hashSets.size()>t1.size()) return -1;
                 return 1;
             }
         });
 
+        int counter = 1;
+        int bigCounter=0;
         try {
             FileWriter fileWriter = new FileWriter(resultFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter, 1024*4);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter, 1024 * 4);
 
             //write result
-            long counter = 1;
-            for (ArrayList<String> list : dic){
-                bufferedWriter.write("Group "+ (counter++)+":\n");
-                for (String s : list){
-                    bufferedWriter.write(s+"\n");
+
+            for (HashSet<HashSet<String>> gr : sizedList){
+                bufferedWriter.write("Group "+(counter++)+":\n");
+
+                if(gr.size()>1) bigCounter++;
+
+                for (HashSet<String> str : gr){
+
+                    for (String word : str){
+                        bufferedWriter.write(word+" ");
+                    }
+                    bufferedWriter.write("\n");
                 }
+
             }
 
 
-//            Map<String, HashSet<String>> sorted_map = sortByValue(map);
-
-//            for (Map.Entry<String, HashSet<String>> entry : sorted_map.entrySet()) {
-//
-//                //print group
-//                bufferedWriter.write("Group "+counter+":\n");
-//                for ()
-//
-//            }
-
-//            LinkedHashMap<String,HashSet<String>> we;
-//            List<Group> list = new ArrayList<>(groups);
-//            list.sort(new Comparator<Group>() {
-//                @Override
-//                public int compare(Group group, Group t1) {
-//                    return Integer.compare(group.size(), t1.size());
-//                }
-//            });
-//            Collections.reverse(list);
-//
-//            for (Group g : list){
-//                if(g.size()>1){
-//                    bigGroupCounter++;
-//                }
-////                fileWriter.write("Group: "+(counter++)+":\n");
-////                fileWriter.write(g.toString());
-//
-////                System.out.println("Group: "+(counter++)+":\n");
-////                System.out.println(g.toString());
-//
-//                bufferedWriter.write("Group: "+(counter++)+":\n");
-//                bufferedWriter.write(g.toString());
-//            }
-
-
-
         } catch (IOException e) {
-            System.out.println("Cant write to file!");
             e.printStackTrace();
-            return;
         }
-        System.out.println("Groups with more than 2 strings: "+ bigGroupCounter);
         long endTime = System.nanoTime();
+        double epsTime = (double)(endTime - startTime)/1_000_000_000;
+        System.out.println("All groups: "+counter );
+        System.out.println("Big groups: "+bigCounter );
+        System.out.println("Time: "+epsTime);
 
-        System.out.println( (double)(endTime-startTime)/1_000_000_000 );
     }
-
-
-//    private static Map<String, HashSet<String>> sortByValue(Map<String, HashSet<String>> unsortMap)
-//    {
-//        List<Map.Entry<String, HashSet<String>>> list = new LinkedList<>(unsortMap.entrySet());
-//
-//
-//        list.sort((stringHashSetEntry, t1) -> {
-//            if (stringHashSetEntry.getValue().size() == t1.getValue().size()) return 0;
-//            if (stringHashSetEntry.getValue().size()>t1.getValue().size()) return -1;
-//            return 1;
-//        });
-//        // Sorting the list based on values
-////        list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
-////                ? o1.getKey().compareTo(o2.getKey())
-////                : o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
-////                ? o2.getKey().compareTo(o1.getKey())
-////                : o2.getValue().compareTo(o1.getValue()));
-//        return list.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new));
-//
-//    }
-//
-//    private static void printMap(Map<String, Integer> map)
-//    {
-//        map.forEach((key, value) -> System.out.println("Key : " + key + " Value : " + value));
-//    }
 }
 
 
---
+++ src/main/java/org/ing/Node.java
@@ -0,0 +1,43 @@
+package org.ing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Node {
+    private HashSet<Node> connected = new HashSet<>();
+    private HashSet<String> string;
+    boolean wrapped = false;
+
+    public Node(HashSet<String> s){
+        string = s;
+    }
+
+    public void getConnectList(HashSet<HashSet<String>> set){
+        set.add(string);
+        wrap();
+        for (Node n : connected){
+            if(!n.isWrapped()){
+                n.getConnectList(set);
+            }
+
+        }
+    }
+
+    public void connect(Node other){
+        connected.add(other);
+        other.safeConnect(this);
+    }
+
+    public void safeConnect(Node other){
+        connected.add(other);
+    }
+
+    public boolean isWrapped(){
+        return wrapped;
+    }
+    public void wrap(){
+        wrapped = true;
+    }
+
+}
